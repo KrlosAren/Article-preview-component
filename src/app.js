2dd527867b776ed6939239708e89c4423f35fbd9
Carlos Lopez Z <krlosaren@gmail.com>
2020-08-27 22:42:58 -0400

    almost finish

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 import './styles/main.styl';
+
+const button = document.getElementById('button');
+const share = document.getElementById('share');
+const rrss = document.querySelectorAll('.rrss');
+
+rrss.forEach((item) => {
+  item.addEventListener('click', () => {
+    share.classList.remove('onVisible');
+    button.classList.remove('active');
+  });
+});
+
+button.addEventListener('click', () => {
+  if (window.innerWidth < 425) {
+    if (share.classList.contains('onVisible')) {
+      share.classList.remove('onVisible');
+      button.style.backgroundColor = 'hsl(217, 19%, 35%)';
+    } else {
+      share.classList.add('onVisible');
+      button.style.backgroundColor = 'hsl(214, 17%, 51%)';
+    }
+  } else {
+    if (share.classList.contains('onVisible')) {
+      share.classList.remove('onVisible');
+      button.classList.remove('active');
+    } else {
+      share.classList.add('onVisible');
+      button.classList.add('active');
+    }
+  }
+});
+
